1e790cdf6cdade4f079a2f7b1409c24936a74be4
Mawadda Al Khorchani <alkhorchanimawadda@gmail.com>
2026-05-18 07:56:29 +0200

    Finished making About me text editor, now just the about me image uploader. Need to generate sharable url from the files in the backend.

--- UIAdmin/src/AboutMe.jsx
+++ UIAdmin/src/AboutMe.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react-hooks/set-state-in-effect */
 import { Link } from 'react-router-dom';
-import { useState , useEffect} from 'react';
+import { useState , useEffect, useRef} from 'react';
 import { gql } from "@apollo/client";
-import { useQuery, useMutation} from "@apollo/client/react";
+import { useQuery, useMutation } from "@apollo/client/react";
+import DefaultImage from './assets/uploadImage.jpg';
 import './AboutMe.css';
 
 const GETABTMETEXT = gql`query{
@@ -11,25 +12,76 @@ const GETABTMETEXT = gql`query{
     text
   }
 }`;
+const GET_IMAGE = gql`
+query{
+    readAbtMeText
+    {
+        imageUrl
+    }
+}
+`;
 
-const EditAboutMeText = (props) =>
-{
+function useImageUpload() {
+    const { data } = useQuery(GET_IMAGE);
+    const [imgUrl, setImageUrl] = useState(DefaultImage);
+
+    useEffect(() => {
+        if (data?.readAbtMeText?.imageUrl) {
+            setImageUrl(data.readAbtMeText.imageUrl);
+        }
+    }, [data?.readAbtMeText?.imageUrl]);
+
+    return imgUrl;
+}
+
+function AboutMe() {
     const { data } = useQuery(GETABTMETEXT);
-    const [aboutMeText, setAboutMeText] = useState();
-    const [aboutMeImg, setAboutMeImg] = useState(
-        "https://i.pinimg.com/1200x/00/a8/bd/00a8bdcdb6a65ba1dc9a2f325e1390fc.jpg"
-    );
+    const image = useImageUpload();
+    const [aboutMeText, setAboutMeText] = useState("");
+    const [abtMeImg, setAbtMeImg] = useState(image);
+    const [showTextArea, setShowTextArea] = useState(false);
+    const [buttonText, setButtonText] = useState("Edit text");
 
     useEffect(() => {
         if (data?.readAbtMeText?.text) {
             setAboutMeText(data.readAbtMeText.text);
         }
     }, [data?.readAbtMeText?.text]);
+
+    useEffect(() => {
+        setAbtMeImg(image);
+    }, [image]);
+
+    const handleText = () => {
+        if (buttonText === "Edit text") {
+            setButtonText("Save text");
+            setShowTextArea(true);
+        } else if (buttonText === "Save text") {
+            handleSubmit();
+            setButtonText("Edit text");
+            setShowTextArea(false);
+        }
+    };
+
+    const fileUploadRef = useRef();
+    const handleImageUpload = (event) => {
+        event.preventDefault();
+        fileUploadRef.current.click();
+    }
+
+    const handleImageDisplay = () => {
+        const uploadedFile = fileUploadRef.current.files[0];
+
+        const cachedImg = URL.createObjectURL(uploadedFile);
+        setAbtMeImg(cachedImg);
+    }
+
     const INSERTABTMETEXT = gql`
         mutation PutTextInJson($abtMeText: AboutMeTextInput!) {
         putTextInJson(abtMeText: $abtMeText)
         }
     `;
+
     const [insertAbtMeText] = useMutation(INSERTABTMETEXT);
 
     const handleSubmit = async () => {
@@ -37,70 +89,60 @@ const EditAboutMeText = (props) =>
             variables: {
                 abtMeText: {
                     text: aboutMeText,
-                    imageUrl: aboutMeImg
+                    imageUrl: abtMeImg
                 }
             }
         });
     };
 
-    if (props.showTextArea)
-    {
-        return(<>
-        <section>
-            <textarea
-                id="about-me-text"
-                value={aboutMeText}
-                onChange={e => setAboutMeText(e.target.value)}
-                maxLength={1000}>
-            </textarea>
-            <button
-                id="submit-btn"
-                onClick={handleSubmit}
-            >Save</button>
-            </section></>
-            )
-    }
-}
-
-const ShowTextUnedited = (props) => {
-    const { data } = useQuery(GETABTMETEXT);
-    const [aboutMeText, setAboutMeText] = useState("Huh");
-    const [aboutMeImg, setAboutMeImg] = useState(
-        "https://i.pinimg.com/1200x/00/a8/bd/00a8bdcdb6a65ba1dc9a2f325e1390fc.jpg"
-    );
-
-    useEffect(() => {
-        if (data?.readAbtMeText?.text) {
-            setAboutMeText(data.readAbtMeText.text);
-        }
-    }, [data?.readAbtMeText?.text]);
-    if (!props.showTextArea) {
-        return (<article id= "about-me-text-unedited">{aboutMeText}</article>)
-    }
-}
-function AboutMe() {
-    const [showTextArea, setShowTextArea] = useState(false);
-
     return (
         <>
-        <header>
+            <header>
+                <div className="about-container">
+                    <h2 id="about">About</h2>
+                    <h2 id="about2">me</h2>
+                </div>
             <nav className="nav-container">
                 <Link className="nav-btn" to="/">Home</Link>
                 <Link className="nav-btn" to="/Resume">Resumé</Link>
                 <Link className="nav-btn" to="/Projects">Projects</Link>
-            </nav>
-                <h1>About me</h1>
+                </nav>
         </header>
             <section>
-                <button onClick={() => showTextArea == true ? setShowTextArea(false) : setShowTextArea(true)}>Edit text</button>
-                <ShowTextUnedited showTextArea={showTextArea} />
-                <EditAboutMeText showTextArea={showTextArea} />
-        </section>
-        <aside>
-        </aside>
+                <button id="show-text-btn" onClick={handleText}>{buttonText}</button>
+                {showTextArea ? (
+                    <article>
+                        <textarea
+                            id="about-me-text"
+                            value={aboutMeText}
+                            onChange={(e) => setAboutMeText(e.target.value)}
+                            maxLength={5000}
+                        />
+                    </article>
+                ) : (
+                    <article id="about-me-text-unedited">
+                        {aboutMeText}
+                    </article>
+                )}
+                <aside>
+                    <img id="about-me-img" src={abtMeImg} alt="about me image."/>
+                    <form>
+                        <button
+                            id="edit-img-btn"
+                            type="submit"
+                            onClick={handleImageUpload}
+                        >Choose file</button>
+                        <input
+                            type="file"
+                            ref={fileUploadRef}
+                            onChange={handleImageDisplay}
+                            hidden>
+                        </input>
+                    </form>
+                </aside>
+            </section>
         </>
     )
 }
-
 
 export default AboutMe;
