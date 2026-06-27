8a1092d2db5fc2f8d52ab53c64cb09b217b9d18d
Mawadda Al Khorchani <alkhorchanimawadda@gmail.com>
2026-06-27 23:49:35 +0200

    Minimal API still uses old host somehow?

--- Endpoints/UploadEndPoints.cs
+++ Endpoints/UploadEndPoints.cs
@@ -32,7 +32,7 @@ namespace PortfolioWebsite.Endpoints
                 await file.CopyToAsync(stream);
 
                 var imageUrl =
-                    $"{request.Scheme}://localhost:8081/imguploads/{fileName}";
+                    $"{request.Scheme}://{request.Host}/imguploads/{fileName}";
 
                 return Results.Ok(new
                 {
